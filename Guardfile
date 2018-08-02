14724a5fb0080f2e3f89de95b08bf8eab8cadb1a
Bhagath <bhagath@sproutssolutions.com>
2018-08-02 12:42:47 +0530

    guard file modification and edit update feature test for article bootstrap stylying for articles

--- Guardfile
+++ Guardfile
@@ -44,6 +44,9 @@ guard :rspec, cmd: "rspec" do
   rails = dsl.rails(view_extensions: %w(erb haml slim))
   dsl.watch_spec_files_for(rails.app_files)
   dsl.watch_spec_files_for(rails.views)
+  watch(%r{^app/controllers/(.+)_(controller)\.rb$})  { "spec/features" }
+  watch(%r{^app/models/(.+)\.rb$})  { "spec/features" }
+
 
   watch(rails.controllers) do |m|
     [
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
+  
   def index
   	@articles = Article.all
   end
+
   def new
   	@article = Article.new
   end
+
   def create
   	@article = Article.new(article_params)
   	if @article.save
@@ -15,9 +18,26 @@ class ArticlesController < ApplicationController
   		render :new
   	end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article has been updated"
+      redirect_to @article
+    else
+      flash.now[:danger] = "Article has not been updated"
+      render :edit
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
+
   private
   def article_params
   	params.require(:article).permit(:title,:body)
